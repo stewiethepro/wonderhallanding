00089be88bc3293e43dd46f733825db9489b089a
wonderhaldev <119766866+wonderhaldev@users.noreply.github.com>
2022-12-12 11:11:52 +1300

    hero edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export function Hero() {
   return (
     <Container className="pt-20 pb-16 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        Write better with {' '}
+        Write better copy with {' '}
         <span className="relative whitespace-nowrap text-blue-600">
           <svg
             aria-hidden="true"
