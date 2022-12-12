28cab06f72f40e0a82bb1b907091b40610031ddd
wonderhaldev <119766866+wonderhaldev@users.noreply.github.com>
2022-12-12 21:12:29 +1300

    final commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>TaxPal - Accounting made simple for small businesses</title>
+        <title>WonderHal - Idea to content in 30 seconds</title>
         <meta
           name="description"
           content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
